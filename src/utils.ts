3ab5342e5a520bb94c1874b3f86858789adbf4d6
felixgro <me@felixgrohs.dev>
2021-12-01 20:51:30 +0100

    init

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+export const createRouter = (routes: { [key: string]: () => Promise<any> }): void => {
+    const getRoute = (path: string) => {
+        return routes[path] || routes['/'];
+    }
+
+    getRoute(window.location.pathname).call({});
+}
+
+export const getContext = (): WebGLRenderingContext => {
+    const canvas = document.querySelector('canvas');
+    if (!canvas) throw new Error('Canvas not found');
+    let gl: WebGLRenderingContext | RenderingContext | null = canvas.getContext('webgl');
+    if (!gl) gl = canvas.getContext('experimental-webgl');
+    if (!gl) throw new Error('WebGL not supported');
+    return gl as WebGLRenderingContext;
+}
